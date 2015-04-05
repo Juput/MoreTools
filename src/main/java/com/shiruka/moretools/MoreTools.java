09ff5b65de9105e1887163dbd0be451e708c8f60
Shiruka <mallorcaguille@gmail.com>
2015-04-05 12:30:16 +0200

    modification of the mod class

--- src/main/java/com/shiruka/moretools/MoreTools.java
+++ src/main/java/com/shiruka/moretools/MoreTools.java
@@ -1,8 +1,28 @@
 package com.shiruka.moretools;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="MoreTools", name="More Tools", version="1.7.10-1.0")
 public class MoreTools
 {
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
